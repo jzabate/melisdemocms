2910c07ec30e21885cc1c966bd59dcf8efd2e4b0
Junry Zabate <jzabajte@melistechnology.com>
2018-08-07 12:04:27 +0800

    code update

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,108 @@
+// config object
+// jsConcatFiles = list of javascript files  ( in order ) to concatenate
+// buildFilesFoldersRemove = list of files to remove when running final build
+var config = {
+	jsConcatFiles: [
+		'public/js/vendor/jquery-2.2.4.min.js',
+		'public/js/bootstrap.min.js',
+		'public/js/owl.carousel.js',
+		'public/js/jquery.countdown.min.js',
+		'public/js/plugins.js',
+		'public/js/main.js',
+		'public/js/MelisPlugins/MelisDemoCms.MelisCmsSliderShowSliderPlugin.init.js',
+		'public/js/MelisPlugins/MelisDemoCms.MelisCmsNewsLatestNewsPlugin.init.js',
+		'public/js/MelisPlugins/MelisDemoCms.MelisFrontShowListFolderPlugin.init.js'
+	],
+	cssConcatFiles: [
+		'public/css/bootstrap.min.css',
+		'public/css/core.css',
+		'public/css/shortcode/shortcode.css',
+		'public/css/owl.carousel.css',
+		'public/css/owl.theme.default.css',
+		'public/css/owl.theme.green.min.css',
+		'public/css/style.css',
+		'public/css/responsive.css',
+		'public/css/animate.css',
+		'public/css/custom.css',
+		'public/css/skin/skin-default.css'
+	],
+	buildFilesFoldersRemove: [
+		'public/build/js/!(main.min.js)',
+		'public/build/css/!(main.min.css)'
+	]
+}
+
 // required
 var gulp 			= require('gulp'),
 	uglify 			= require('gulp-uglify'), // js minifier
-	plumber 		= require('gulp-plumber'), // error management
-	//jshint 			= require('gulp-jshint'), //javascript hint
-	//tinyimg 		= require('gulp-tinifier'), // image compressor with the use of https://tinypng.com service
-	//concat			= require('gulp-concat'), // concatenates files
+	sourcemaps 		= require('gulp-sourcemaps'), //creates sourcemaps
+	concat			= require('gulp-concat'), // concatenates files
 	rename 			= require('gulp-rename'); // rename files using some transformers
-	//uglyfycss		= require('gulp-uglifycss'), // css minifier
-	//autoprefixer 	= require('gulp-autoprefixer'), // css prefixer
-	//del 			= require('del'); // delete files and folders using globs
-	//browserSync		= require('browser-sync'), // browser sync on any other devices
-	//reload 			= browserSync.reload;
+	uglyfycss		= require('gulp-uglifycss'), // css minifier
+	autoprefixer 	= require('gulp-autoprefixer'), // css prefixer
+	del 			= require('del'); // delete files and folders using globs
+
+	// log errors
+	function errorLog(error) {
+		console.log(error.message);
+		this.emit('end');
+	}
 
 	// script task
 	gulp.task('scripts', function() {
-		gulp.src(['public/js/**/*.js', '!public/js/**/*.min.js'])
-			.pipe(plumber())
-			.pipe(rename({suffix: '.min'}))
-			.pipe(uglify())
-		.pipe(gulp.dest('public/js/build'));
+		return gulp.src(config.jsConcatFiles)
+			.pipe(sourcemaps.init())
+				.pipe(concat('temp.js'))
+				.pipe(uglify())
+				.on('error', errorLog)
+				.pipe(rename('main.min.js'))
+			.pipe(sourcemaps.write('./'))
+			.pipe(gulp.dest('public/build/js'));
+	});
+
+	// style task
+	gulp.task('styles', function() {
+		return gulp.src(config.cssConcatFiles)
+			.pipe(sourcemaps.init())
+				.pipe(concat('temp.css'))
+				.pipe(uglyfycss())
+				.on('error', errorLog)
+				.pipe(autoprefixer({
+					browsers: ['last 3 versions'],
+					cascade: false
+				}))
+				.pipe(rename('main.min.css'))
+			.pipe(sourcemaps.write('./'))
+			.pipe(gulp.dest('public/build/css'));
+	});
+
+	// build tasks
+	// task to clean all files and folder inside the build folder
+	gulp.task('build:cleanfolder', function(cb) {
+		del([
+			'public/build/**'
+		], cb);
+	});
+
+	// task to create build directory for all js & css files
+	gulp.task('build:copy', ['build:cleanfolder'], function() {
+		return gulp.src(['public/css/**/*', 'public/js/**/*'])
+				   .pipe(gulp.dest('public/build/'))
+	});
+
+	// task to remove unwanted files, related to buildFilesFoldersRemove
+	gulp.task('build:remove', ['build:copy'], function(cb) {
+		del(config.buildFilesFoldersRemove, cb);
+	});
+
+	// default build task
+	gulp.task('build', ['build:copy', 'build:remove']);
+
+	// watch tasks
+	gulp.task('watch', function() {
+		gulp.watch('public/js/**/*.js', ['scripts']);
+		gulp.watch('public/css/**/*.css', ['styles']);
 	});
 
 	// default task
-	gulp.task('default', ['scripts']);
+	gulp.task('default', ['scripts', 'styles', 'watch']);
